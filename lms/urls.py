e353a4451172f4c040284a47e8489d71a55bd8af
VladislavaFn <vladislava.fridmann1@aol.com>
2026-03-16 21:27:19 +0300

    11

--- lms/urls.py
+++ lms/urls.py
@@ -1,10 +1,10 @@
-from rest_framework.routers import DefaultRouter
-from django.urls import path, include
+#from rest_framework.routers import DefaultRouter
+#from django.urls import path, include
 
-from lms.views import CourseViewSet, LessonViewSet  # предполагаю что они у тебя есть
+#from lms.views import CourseViewSet, LessonViewSet  # предполагаю что они у тебя есть
 
-router = DefaultRouter()
-router.register(r'courses', CourseViewSet, basename='course')
-router.register(r'lessons', LessonViewSet, basename='lesson')
+#router = DefaultRouter()
+#router.register(r'courses', CourseViewSet, basename='course')
+#router.register(r'lessons', LessonViewSet, basename='lesson')
 
-urlpatterns = router.urls
+#urlpatterns = router.urls
