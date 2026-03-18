1c35b10c0163d91826e83d76c543afc131745af2
VladislavaFn <vladislava.fridmann1@aol.com>
2026-03-18 21:03:07 +0300

    16

--- lms/views.py
+++ lms/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Payment
+from users.models import Payment
 from .tasks import send_course_update_email
 
 from users.permissions import IsModerator, IsOwnerOrModerator
